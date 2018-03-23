9a015e8233985cbabf9b4ee2def18bd288777c28
Unknown <drewdemarest@gmail.com>
2018-03-22 18:08:04 -0700

    DEVEL TEST: In Main can now import various CSVs instead of 2.

--- main.cpp
+++ main.cpp
@@ -1,14 +1,8 @@
 #include "mainwindow.h"
 #include "importer/charliescsvimporter.h"
 #include "entity/order/orderfactory.h"
-
+#include "entity/order/order.h"
 #include <QApplication>
-
-
-//QMap<int, Order> filter(QMap<int, Order> &result, QList<int> &sharedKeys)
-//{
-//    qDebug << "potato";
-//}
 
 int getInvoiceNumbers(QPair<int, QVariantMap> pair)
 {
@@ -21,40 +15,73 @@ int main(int argc, char *argv[])
     MainWindow w;
     w.show();
 
-    CharliesCSVImporter importer;
-    importer.setCSVPath(QCoreApplication::applicationDirPath() + "/orderTrackingHistory.csv");
-    QList<QPair<int, QVariantMap>> pairList = importer.importCharliesCSVAsPair(CharliesCSVImporter::reportTypes::OrderTrackingHistory);
-    importer.setCSVPath(QCoreApplication::applicationDirPath() + "/routeProfitabilityDetail.csv");
-    QList<QPair<int, QVariantMap>> pairList2 = importer.importCharliesCSVAsPair(CharliesCSVImporter::reportTypes::RouteProfitability);
-
-    //Surely this is a better way to do this.
-    QList<int> keys0 = QtConcurrent::blockingMapped(pairList, getInvoiceNumbers);
-    QList<int> keys1 = QtConcurrent::blockingMapped(pairList2, getInvoiceNumbers);
-
+    QList<QList<QPair<int, QVariantMap>>> importResults;
+    QList<QList<int>> keys;
+    QList<QList<int>> deleteFromKeys;
     QList<int> sharedKeys;
+    QList<int> tempKeys;
 
-    QList<int> deleteFromKeys0;
-    QList<int> deleteFromKeys1;
 
-    std::sort(keys0.begin(), keys0.end());
-    std::sort(keys1.begin(), keys1.end());
-    std::set_intersection(keys0.begin(), keys0.end(), keys1.begin(), keys1.end(), std::back_inserter(sharedKeys));
+    CharliesCSVImporter importer;
+
+    importer.setCSVPath(QCoreApplication::applicationDirPath() + "/orderHistorySummary0.csv");
+    importResults.append(importer.importCharliesCSVAsPair(CharliesCSVImporter::reportTypes::OrderTrackingHistory));
+
+    importer.setCSVPath(QCoreApplication::applicationDirPath() + "/orderHistorySummary1.csv");
+    importResults.append(importer.importCharliesCSVAsPair(CharliesCSVImporter::reportTypes::OrderTrackingHistory));
+
+    importer.setCSVPath(QCoreApplication::applicationDirPath() + "/orderHistorySummary2.csv");
+    importResults.append(importer.importCharliesCSVAsPair(CharliesCSVImporter::reportTypes::OrderTrackingHistory));
+
+    importer.setCSVPath(QCoreApplication::applicationDirPath() + "/routeProfitabilityDetail0.csv");
+    importResults.append(importer.importCharliesCSVAsPair(CharliesCSVImporter::reportTypes::RouteProfitability));
+
+    importer.setCSVPath(QCoreApplication::applicationDirPath() + "/routeProfitabilityDetail1.csv");
+    importResults.append(importer.importCharliesCSVAsPair(CharliesCSVImporter::reportTypes::RouteProfitability));
+
+    for(auto importResult: importResults)
+        keys.append(QtConcurrent::blockingMapped(importResult, getInvoiceNumbers));
+
+    for(int i = 0; i < keys.length(); ++i)
+        std::sort(keys[i].begin(), keys[i].end());
+
+    sharedKeys = *keys.begin();
+    for(int i = 0; i < keys.length() - 1 ; ++i)
+    {
+        std::set_intersection(sharedKeys.begin(), sharedKeys.end(), keys[i+1].begin(), keys[i+1].end(), std::back_inserter(tempKeys));
+        sharedKeys = tempKeys;
+        tempKeys.clear();
+    }
     std::sort(sharedKeys.begin(), sharedKeys.end());
-    std::set_difference(keys0.begin(), keys0.end(), sharedKeys.begin(), sharedKeys.end(), std::back_inserter(deleteFromKeys0));
-    std::set_difference(keys1.begin(), keys1.end(), sharedKeys.begin(), sharedKeys.end(), std::back_inserter(deleteFromKeys1));
-    //End section that is tantalizingly improveable.
+    qDebug() << sharedKeys.size();
+    for(int i = 0; i < keys.length(); ++i)
+    {
+        deleteFromKeys.append(QList<int>());
+        std::set_difference(keys[i].begin(), keys[i].end(), sharedKeys.begin(), sharedKeys.end(), std::back_inserter(deleteFromKeys[i]));
+    }
 
-    pairList.append(pairList2);
+    QList<QPair<int, QVariantMap>> completeImport;
+    QList<int> completeDelete;
+    for(auto importResult: importResults)
+        completeImport.append(importResult);
+
+    for(auto deleteKeys:deleteFromKeys)
+        completeDelete.append(deleteKeys);
+
 
     QMap<int, Order> orderMap;
-    for(auto pair: pairList)
+    for(auto pair: completeImport)
         orderMap[pair.first].addInformation(pair.second);
-    deleteFromKeys0.append(deleteFromKeys1);
-    std::sort(deleteFromKeys0.begin(), deleteFromKeys0.end());
-    for(auto key:deleteFromKeys0)
+
+    std::sort(completeDelete.begin(), completeDelete.end());
+
+    for(auto key:completeDelete)
         orderMap.remove(key);
 
     qDebug() << orderMap.size();
+    for(auto order:orderMap)
+        qDebug() << order.extractInformation();
+
 
     return a.exec();
 }
